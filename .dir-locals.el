9b3d8e471a8ab5280898095dcef1b77cf7570241
Aryadev Chavali <aryadev@aryadevchavali.com>
2025-02-11 00:42:49 +0000

    Some dir-locals for your (my) convenience.

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((compile-command . "./build")
+         (+license/license-choice . "GNU General Public License Version 2"))))
